7233067b8e4ac9ab8511d78295861d390be4f025
declspecl <gavind2559@gmail.com>
2025-07-16 20:21:11 -0400

    more logging testing

--- src/main/java/com/sourceplot/handler/RepoAnalysisHandler.java
+++ src/main/java/com/sourceplot/handler/RepoAnalysisHandler.java
@@ -7,9 +7,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
@@ -20,6 +17,9 @@ import com.sourceplot.init.EnvironmentModule;
 import com.sourceplot.init.ServiceModule;
 import com.sourceplot.model.ActiveRepositoriesPayload;
 import com.sourceplot.model.RepoStats;
+
+import lombok.extern.log4j.Log4j2;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
@@ -27,9 +27,8 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 
 import static com.google.inject.Guice.createInjector;
 
+@Log4j2
 public class RepoAnalysisHandler implements RequestHandler<SQSEvent, SQSBatchResponse> {
-    private static final Logger LOG = LogManager.getLogger(RepoAnalysisHandler.class);
-
     @Inject
     private ObjectMapper objectMapper;
     @Inject
@@ -44,16 +43,17 @@ public class RepoAnalysisHandler implements RequestHandler<SQSEvent, SQSBatchRes
 
     @Override
     public SQSBatchResponse handleRequest(SQSEvent input, Context context) {
-        context.getLogger().log("Test lambda context logger");
-        LOG.info("Test log4j2 logger");
+        log.info("Received request with {} messages", input.getRecords().size());
 
         var failures = new ArrayList<SQSBatchResponse.BatchItemFailure>();
 
         for (SQSEvent.SQSMessage message : input.getRecords()) {
+            log.info("Processing message: {}", message.getMessageId());
+
             try {
                 processMessage(message);
             } catch (Exception e) {
-                LOG.error("Error processing message: {}", e.getMessage());
+                log.error("Error processing message: {}", e.getMessage());
                 failures.add(new SQSBatchResponse.BatchItemFailure(message.getMessageId()));
             }
         }
@@ -63,12 +63,12 @@ public class RepoAnalysisHandler implements RequestHandler<SQSEvent, SQSBatchRes
 
     private void processMessage(SQSEvent.SQSMessage message) throws InterruptedException, IOException, JsonProcessingException {
         var payload = objectMapper.readValue(message.getBody(), ActiveRepositoriesPayload.class);
-        LOG.info("Processing payload: {}", payload);
+        log.info("Processing payload: {}", payload);
 
         for (var repository : payload.repositories()) {
             var languagesUri = URI.create(String.format("https://api.github.com/repos/%s/languages", repository.name()));
             var languagesResponse = httpClient.send(HttpRequest.newBuilder().uri(languagesUri).build(), HttpResponse.BodyHandlers.ofString());
-            LOG.info("Languages response: {}", languagesResponse.body());
+            log.info("Languages response: {}", languagesResponse.body());
 
             repoStatsAccessor.saveRepoStats(
                 RepoStats.builder()
